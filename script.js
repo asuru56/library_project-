c754044b01a5537586d01b122289fc4b86a8c6c7
asuru56 <56asuru@gmail.com>
2023-11-18 18:56:35 +0530

    Working Add and Submit Button

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-}
+};
 
 function addBookToLibrary(title, author, pages, isRead) {
     // Create a new book and add it to the library
     const newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
-}
-let i = 0 ; 
-function displayBooks(){
-        const book = myLibrary[i++];
-        console.log(book.title, book.author, book.pages, book.isRead);
-}
+};
 
 
-function deletethisBook(){
-    myLibrary.pop();
-    displayBooks(); 
-}
-
-function submitForm(){
+let submitForm = document.querySelector("#submit");
+submitForm.addEventListener("click",function(){
     var name = document.getElementById("inputName").value;
     var author = document.getElementById("inputAuthor").value;
     var pages = document.getElementById("pages").value;
     var read = document.getElementById("isRead").value;
     addBookToLibrary(name, author, pages, read);
-    displayBooks();
      alert('Form submitted!');
-}
+     var page = document.querySelector("#inputForm");
+    page.style.display = "none"
+
+}); 
+
+let newBook = document.querySelector("#new_button") ;
+
+newBook.addEventListener("click", function(){
+    var page = document.querySelector("#inputForm");
+    page.style.display = "block"; 
+})
