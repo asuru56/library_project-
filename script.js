0f2e24844a0b68b1216c59c7e929b2d83cd09a54
asuru56 <56asuru@gmail.com>
2023-11-18 16:00:03 +0530

    Updated

--- script.js
+++ script.js
@@ -13,30 +13,23 @@ function addBookToLibrary(title, author, pages, isRead) {
     const newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
 }
-
-function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        const book = myLibrary[i];
+let i = 0 ; 
+function displayBooks(){
+        const book = myLibrary[i++];
         console.log(book.title, book.author, book.pages, book.isRead);
-        var table = document.getElementById("myTable");
-        var row = table.insertRow(i + 1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        cell1.innerHTML = book.title;
-        cell2.innerText = book.author;
-        cell3.innerText = book.pages;
-        cell4.innerText = book.isRead;
-    }
+}
 }
 
+function deletethisBook(){
+    myLibrary.pop();
+    displayBooks(); 
+}
 
-function submitForm() {
+function submitForm(){
     var name = document.getElementById("inputName").value;
     var author = document.getElementById("inputAuthor").value;
     var pages = document.getElementById("pages").value;
-    var read = document.getElementById("iRead").value;
+    var read = document.getElementById("isRead").value;
     addBookToLibrary(name, author, pages, read);
     displayBooks();
      alert('Form submitted!');
